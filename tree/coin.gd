386735e69ad2130aa80e6e52108c7dc0f2881657
Ian Atherton <atherton.i@gmail.com>
2022-08-09 08:38:54 -0700

    rotating cloud objects

---
+++ tree/coin.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _process(delta):
+	
+	rotation += 1 * delta
+	
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass

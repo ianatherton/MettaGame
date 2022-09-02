3e176d47df28db40ff06e61140d0f0860579f084
Ian Atherton <atherton.i@gmail.com>
2022-09-01 23:36:27 -0700

    add check for which scene

--- tree/scripts/wisp.gd
+++ tree/scripts/wisp.gd
@@ -1,14 +1,17 @@
 extends Area2D
 
 func _on_Wisp_body_entered(body):
+	var context = get_tree().get_current_scene().filename #load name of current level scene
+	#print(context)  #check name of file name
 	if body.get_name() == "player": #check name of body collision
-		GameManager.wisps = GameManager.wisps + 1 #increment global var +1
-		$AnimatedSprite.play("fade") #play fade animation for wisp
-		if GameManager.metta.size() > GameManager.wisps: #check if array size is big enough for GameManager.wisps index
-			#GameManager.wisps = GameManager.wisps + 1
-			#print(GameManager.metta)
-			print(GameManager.wisps)
-			$Label._setword(GameManager.wisps)
-			$AnimatedSprite.play("fade")
-			$AnimatedSprite.rotspd = 0
-			$CollisionShape2D.queue_free()
+		if context == "res://levels/treescene1.tscn":
+			GameManager.wisps = GameManager.wisps + 1 #increment global var +1
+			$AnimatedSprite.play("fade") #play fade animation for wisp
+			if GameManager.metta.size() > GameManager.wisps: #check if array size is big enough for GameManager.wisps index
+				#GameManager.wisps = GameManager.wisps + 1
+				#print(GameManager.metta)
+				print(GameManager.wisps)
+				$Label._setword(GameManager.wisps)
+				$AnimatedSprite.play("fade")
+				$AnimatedSprite.rotspd = 0
+				$CollisionShape2D.queue_free()

6efd0b6c45ab561d44832136cb89f74b88f35a30
Ian Atherton <atherton.i@gmail.com>
2022-09-02 08:25:38 -0700

    make wisps in breathe scene work differently

--- tree/scripts/RichTextLabel.gd
+++ tree/scripts/RichTextLabel.gd
@@ -5,6 +5,12 @@ extends Label
 func _setword(a):
 	#set_text("tester")
 	#check here if 
+	var context = get_tree().get_current_scene().filename
+	print(context)
+	if context == "res://levels/treescene1.tscn": #load name of current level scene
 	#if GameManager.metta.count() > a:
 		set_text(GameManager.metta[a])
+		pass
+	elif context == "res://levels/breathe.tscn":
+		set_text(GameManager.breathemantra[a])
 		pass

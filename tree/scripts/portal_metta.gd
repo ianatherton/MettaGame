ad40ca4ab0380b8cf2acfe11c301e5b91f2e9ae1
Ian Atherton <atherton.i@gmail.com>
2022-09-12 21:22:16 -0700

    both portals working

--- tree/scripts/portal_metta.gd
+++ tree/scripts/portal_metta.gd
@@ -7,15 +7,15 @@ extends Area2D
 
 
 # Called when the node enters the scene tree for the first time.
-#func _ready():
-#	pass # Replace with function body.
+func _ready():
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
 func _on_portal_body_entered(body):
-	var context = get_tree().get_current_scene().filename #load name of current level scene
-	print(context)  #check name of file name
 	if body.get_name() == "player": #check name of body collision
-		get_tree().change_scene("res://levels/breathe.tscn")
+		get_tree().change_scene("res://levels/treescene1.tscn")
